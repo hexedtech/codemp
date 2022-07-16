ede7a7758c1daf7231c9c4812c225a39e6ff7181
alemidev <me@alemi.dev>
2022-07-16 03:46:02 +0200

    feat: made nvim client a feature (for now, enabled by default)

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,32 +1,14 @@
-pub mod manager;
 mod nvim;
 
-use tokio::sync::mpsc;
-use nvim_rs::create::tokio::new_parent;
-
-use manager::ConnectionManager;
-use nvim::NeovimHandler;
+pub mod proto { tonic::include_proto!("workspace"); }
+use proto::workspace_client::WorkspaceClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<(dyn std::error::Error + 'static)>> {
-	let (tx, rx) = mpsc::channel(32);
+	let client = WorkspaceClient::connect("http://[::1]:50051").await?;
 
-	//nvim stuff
-	let handler: NeovimHandler = NeovimHandler::new(tx).await?;
-	let (_nvim, io_handler) = new_parent(handler).await;
-
-	// nvim.call(":echo", vec![Value::from("'***REMOVED***'")]).await.unwrap();
-	let mut mngr = ConnectionManager::new("http://[::1]:50051".to_string(), rx).await?;
-	let _task = tokio::spawn(async move {
-		mngr.process_packets().await
-	});
-
-	// Any error should probably be logged, as stderr is not visible to users.
-	match io_handler.await {
-		Err(err) => eprintln!("Error joining IO loop: {:?}", err),
-		Ok(Err(err)) => eprintln!("Process ended with error: {:?}", err),
-		Ok(Ok(())) => eprintln!("Finished"),
-	}
+	#[cfg(feature = "nvim")]
+	crate::nvim::run_nvim_client(client).await.unwrap();
 
 	Ok(())
 }
