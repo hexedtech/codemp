3f49730e7ed56d00da744bacce06cb3207ef2514
frelodev <frelodev@gmail.com>
2023-12-28 00:20:45 +0100

    list of proto files to build

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	tonic_build::compile_protos("proto/buffer.proto")?;
-	tonic_build::compile_protos("proto/cursor.proto")?;
+	tonic_build::compile_protos("proto/model/cursor.proto")?;
+	tonic_build::compile_protos("proto/model/user.proto")?;
+	tonic_build::compile_protos("proto/buffer_service.proto")?;
+	tonic_build::compile_protos("proto/cursor_service.proto")?;
+	tonic_build::compile_protos("proto/workspace_service.proto")?;
 	Ok(())
 }
