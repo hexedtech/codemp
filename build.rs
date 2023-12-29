62303710205978dc30c8af187eb754e62828d7d3
frelodev <frelodev@gmail.com>
2023-12-29 01:06:28 +0100

    feat: fixed proto and tonic includes
    Co-authored-by: zaaarf <me@zaaarf.foo>
    Co-authored-by: alemi <me@alemi.dev>

--- build.rs
+++ build.rs
@@ -1,15 +1,17 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	tonic_build::configure()
-		.build_server(false)
+		// .build_client(cfg!(feature = "client"))
+		//.build_server(cfg!(feature = "server")) // FIXME if false, build fails????
+		// .build_transport(cfg!(feature = "transport"))
 		.compile(
 			&[
-				"proto/model/cursor.proto",
-				"proto/model/user.proto",
+				"proto/user.proto",
+				"proto/cursor.proto",
 				"proto/buffer_service.proto",
 				"proto/cursor_service.proto",
 				"proto/workspace_service.proto"
 			],
-			&["proto", "proto", "proto","proto", "proto"]
-				)?;
+			&["proto"]
+		)?;
 	Ok(())
  }
