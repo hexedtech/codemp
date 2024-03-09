4694a01c9bdf9e8ab838569881c5ceee1a26659d
zaaarf <me@zaaarf.foo>
2024-03-09 17:20:40 +0100

    fix: don't borrow self as mut to join workspace

--- src/client.rs
+++ src/client.rs
@@ -109,7 +109,7 @@ impl Client {
 	}
 
 	/// join a workspace, returns an [tokio::sync::RwLock] to interact with it
-	pub async fn join_workspace(&mut self, workspace: &str) -> crate::Result<Arc<Workspace>> {
+	pub async fn join_workspace(&self, workspace: &str) -> crate::Result<Arc<Workspace>> {
 		let ws_stream = self.services.workspace.clone().attach(Empty{}.into_request()).await?.into_inner();
 
 		let (tx, rx) = mpsc::channel(256);
