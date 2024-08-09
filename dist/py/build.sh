53cd0068b52237366bf1018b3c139ad22acb2945
cschen <cschen@codemp.dev>
2024-08-09 09:13:14 +0200

    chore(python): temporary modification to the build script to make it easier to test with sublime

--- dist/py/build.sh
+++ dist/py/build.sh
@@ -5,3 +5,10 @@ PYO3_PYTHON="$(pyenv which python)"
 TARGET_EXT="$($PYO3_PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))')"
 
 maturin build -i "$PYO3_PYTHON" --out "$WHEEL_DIR"
+
+CODEMPSUBLIME_DIR="../../../codemp-sublime/bindings/"
+
+wheels=($WHEEL_DIR/*.whl)
+for wheel in $wheels; do
+	cp $wheel "$CODEMPSUBLIME_DIR"
+done
