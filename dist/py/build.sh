f5754ed804f2a26dc290b2b428c6b767d7df4a19
cschen <cschen@codemp.dev>
2024-09-07 19:39:27 +0200

    chore(python): token name more explicit

--- dist/py/build.sh
+++ dist/py/build.sh
@@ -4,5 +4,5 @@ WHEEL_DIR="$ROOT_DIR/wheels"
 PYO3_PYTHON="$(pyenv which python)"
 TARGET_EXT="$($PYO3_PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))')"
 
-MATURIN_PYPI_TOKEN="$(cat ~/.local/secrets/pypi_token)" maturin publish -i "$PYO3_PYTHON" \
+MATURIN_PYPI_TOKEN="$(cat ~/.local/secrets/pypi_codemp_token)" maturin publish -i "$PYO3_PYTHON" \
 --out "$WHEEL_DIR" --non-interactive --repository "pypi"
