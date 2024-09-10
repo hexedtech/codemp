7aa9fbb518a07a21f5c49054c67e3b7e9e5193e4
cschen <cschen@codemp.dev>
2024-09-10 19:09:54 +0200

    feat(python): build wheels with manual specified `__init__.py` fix(python): remove build script, not needed anymore

--- dist/py/build.sh
+++
@@ -1,8 +0,0 @@
-ROOT_DIR="$(pwd)"
-WHEEL_DIR="$ROOT_DIR/wheels"
-
-PYO3_PYTHON="$(pyenv which python)"
-TARGET_EXT="$($PYO3_PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))')"
-
-MATURIN_PYPI_TOKEN="$(cat ~/.local/secrets/pypi_codemp_token)" maturin publish -i "$PYO3_PYTHON" \
---out "$WHEEL_DIR" --non-interactive --repository "pypi"
---
+++ dist/py/src/codemp/__init__.py
@@ -0,0 +1,5 @@
+from .codemp import *
+
+__doc__ = codemp.__doc__
+if hasattr(codemp, "__all__"):
+    __all__ = codemp.__all__
