3da0bfd40a2fdbba21a5c2f4ccdd239d3c3de662
frelodev <frelodev@gmail.com>
2024-09-25 14:09:24 +0200

    chore(js): added glue for version function

--- src/ffi/js/ext.rs
+++ src/ffi/js/ext.rs
@@ -1,8 +1,12 @@
 use napi_derive::napi;
-use crate::ext::hash;
 
 
 #[napi(js_name = "hash")]
 pub fn js_hash(str : String) -> napi::Result<i64>{
-    Ok(hash(str))
+    Ok(crate::ext::hash(str))
+}
+
+#[napi(js_name = "version")]
+pub fn js_version(str : String) -> napi::Result<String>{
+    Ok(crate::version())
 }
